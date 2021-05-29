b02bcd7b93e8235ceea69a9690f8c3cee2785848
Shilohjones194 <76697212+Shilohjones194@users.noreply.github.com>
2021-05-29 14:47:24 -0700

    final touches

---
+++ employee.js
@@ -0,0 +1,10 @@
+const mysql = require('mysql');
+const inquirer = require('inquirer');
+
+//create connection:
+// create MySQL global variables
+// Create main menu
+//function for Adding Employee
+//function add Department
+//function add role/job title
+//Function to remove/Delete employee if mistake was made
